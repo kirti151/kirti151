7820f4a3c52a39f0b0f9d7e4670687c867a86003
kirti151 <kirtichhikara042@gmail.com>
2025-02-14 09:41:53 +0530

    Add files via upload

---
+++ seperate line.java
@@ -0,0 +1,10 @@
+class seprateline
+{
+    public static void main(String[] args) {
+        System.out.println("annu annu@cse a03160 3456346");
+        System.out.println("parul parul@rew p031456 0567475");
+        System.out.println("mahak mahak@def m03478 3525435");
+        System.out.println("ashu ashu@sde d043325 875634");
+
+    }
+}
